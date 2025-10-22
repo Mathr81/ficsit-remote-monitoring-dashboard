9e0de8b685776239d0f691c1382ec91b33f70d96
mathr81 <matheobarthes@gmail.com>
2025-10-22 09:28:15 +0200

    fixed the 404 error + changed the default ip + updated .gitignore

--- src/constants/defaultSettingsData.ts
+++ src/constants/defaultSettingsData.ts
@@ -1,7 +1,7 @@
 import type { SettingsData } from '../types/settingsData';
 
 export const defaultSettingsData: SettingsData = {
-    ip: '51.222.82.108',
-    port: '8081',
+    ip: 'sublima.axenthost.me',
+    port: '59305',
     interval: 5000
 };
